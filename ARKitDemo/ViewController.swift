a2c9f18829eaa63e12b46e175631eeb569f15400
Yin Hua <yin.hua@news.com.au>
2018-11-05 10:32:12 +1100

    Initial Commit

---
+++ ARKitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARKitDemo
+//
+//  Created by Hua, Yin on 5/11/18.
+//  Copyright © 2018 NewsDigital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
